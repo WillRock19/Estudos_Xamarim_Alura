b677fb47da66c9e24d87bb140b4d3e4203cbe347
William Pinto <william.pinto@lambda3.com.br>
2019-01-23 16:51:11 -0200

    Criada classe de domínio; exibindo lista com livros na MainPage

---
+++ AluraApp/AluraApp/Domain/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace AluraApp.Domain
+{
+    public class Book
+    {
+        public string AuthorName { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string name, string author, decimal price)
+        {
+            AuthorName = author;
+            Name = name;
+            Price = price;
+        }
+    }
+}
--- AluraApp/AluraApp/MainPage.xaml.cs
+++ AluraApp/AluraApp/MainPage.xaml.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using AluraApp.Domain;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace AluraApp
 {
     public partial class MainPage : ContentPage
     {
+        public List<Book> Books { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            InitializeBooksCollection();
+
+            //listViewBooks.ItemsSource = Books;
+            Books.Add(new Book("Batman: Terra um 3", "Geoff Johns", 55M));
+
+            BindingContext = this;
+        }
+
+        private void InitializeBooksCollection()
+        {
+            Books = new List<Book>()
+            {
+                new Book("A Torre Negra: O pistoleiro", "Stephen King", 49.90M),
+                new Book("Harry Potter e a câmara secreta", "J.K. Rowling", 56M),
+                new Book("Demolidor: revelado", "Ed brubaker", 70.90M),
+                new Book("Batman: ano um", "Frank Miller", 45.20M)
+            };
         }
     }
 }
